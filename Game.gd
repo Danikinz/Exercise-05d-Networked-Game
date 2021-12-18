938e3f7f6b82addb3d1b399841c02f51a5e7084c
Danikinz <coapuff@gmail.com>
2021-12-17 22:01:08 -0500

    Summary
    Implementation of a networked game with two players!

--- Game.gd
+++ Game.gd
@@ -7,3 +7,9 @@ func _ready():
 	var player1 = preload("res://Player/Player.tscn").instance()
 	player1.global_transform = player1pos.global_transform
 	add_child(player1)
+	
+	var player2 = preload("res://Player/Player.tscn").instance()
+	player2.set_name(str(Global.player2id))
+	player2.set_network_master(Global.player2id)
+	player2.global_transform = player2pos.global_transform
+	add_child(player2)
